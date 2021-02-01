f7c3e4f828ad28820fd54d657154caf94f1fea77
Henrique Roldão <henriqueroldao@rede.ulbra.br>
2021-02-01 12:00:09 -0300

    Muitos testes

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,3 @@ if(window.SimpleSlide) {
     });
 
 }
-
-if(window.SimpleAnime) {
-    new SimpleAnime();
-}
-
-if(window.SimpleForm) {
-    new SimpleForm({
-        form: ".formphp", // seletor do formulário
-        button: "#enviar", // seletor do botão
-        erro: "<div id='form-erro'><p>Um erro ocorreu, tente para o email henriqueroldao@rede.ulbra.br</p></div>", // mensagem de erro
-        sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
-    });
-}
-
